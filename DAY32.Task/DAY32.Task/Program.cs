2c7ea4f2ecb50efe381d75e33423d2ac592d2236
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-02-11 22:01:54 +0400

    Add files via upload

---
+++ DAY32.Task/DAY32.Task/Program.cs
@@ -0,0 +1,60 @@
+﻿using DAY19.Task;
+using System;
+
+namespace DAY32.Task
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            City city = new City();
+
+
+            ChinaCity[] chinaCities = new ChinaCity[0];
+
+            int sum = 0;
+            Console.WriteLine("How many cities will be added ? ");
+            byte count = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string name;
+                do
+                {
+                    Console.Write("Enter city name - ");
+                    name = Console.ReadLine();
+
+                } while (string.IsNullOrEmpty(name));
+
+                string populationStr;
+                int population;
+                do
+                {
+                    Console.Write("  Enter population - ");
+                    populationStr = Console.ReadLine();
+
+                } while (!int.TryParse(populationStr, out population));
+
+                sum += population;
+
+
+                ChinaCity chinaCity = new ChinaCity { Name = name, Population = population };
+
+                Array.Resize(ref chinaCities, chinaCities.Length + 1);
+                chinaCities[chinaCities.Length - 1] = chinaCity;
+
+            }
+
+            Console.WriteLine($"\nPopulation Average : {sum / count}");
+
+
+            Console.WriteLine("\n>>>Cities you added :");
+            for (int i = 0; i < chinaCities.Length; i++)
+            {
+                Console.WriteLine($"Name - {chinaCities[i].Name}, Population - {chinaCities[i].Population}");
+            }
+
+
+        }
+    }
+}
